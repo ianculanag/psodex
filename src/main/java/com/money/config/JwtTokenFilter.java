1df58d3ea6f3ae9d6a804f453e437c59a8da775a
ianculanag <ianculanag@gmail.com>
2022-01-18 10:50:11 +0800

    gac-029 | update jwt

--- src/main/java/com/money/config/JwtTokenFilter.java
+++ src/main/java/com/money/config/JwtTokenFilter.java
@@ -1,6 +1,7 @@
 package com.money.config;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -9,36 +10,52 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.Claims;
+
 public class JwtTokenFilter extends OncePerRequestFilter {
-	
+
+	private static Logger log = LoggerFactory.getLogger(JwtTokenFilter.class);
+
 	private JwtTokenProvider tokenProvider;
-	
+
 	public JwtTokenFilter(JwtTokenProvider tokenProvider) {
 		this.tokenProvider = tokenProvider;
 	}
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+		log.info("JwtTokenFilter : doFilterInternal");
 		String token = request.getHeader("Authorization");
-		try {
-			if (token != null && tokenProvider.validateToken(token)) {
-				SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(token));
-			}
-		} catch (RuntimeException e) {
-			SecurityContextHolder.clearContext();
-			response.setContentType("application/json");
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		if (token != null) {
 			try {
-				response.getWriter().println(new JSONObject().put("exception", "expired or invalid JWT Token" + e.getMessage()));
-			} catch (IOException | JSONException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				Claims claims = tokenProvider.getClaimsFromToken(token);
+				if (!claims.getExpiration().before(new Date())) {
+					Authentication authentication = tokenProvider.getAuthentication(claims.getSubject());
+					if (authentication.isAuthenticated()) {
+						SecurityContextHolder.getContext().setAuthentication(authentication);
+					}
+				}
+			} catch (RuntimeException e) {
+				try {
+					SecurityContextHolder.clearContext();
+					response.setContentType("application/json");
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					response.getWriter().println(
+							new JSONObject().put("exception", "expired or invalid JWT Token" + e.getMessage()));
+				} catch (IOException | JSONException e1) {
+					e1.printStackTrace();
+				}
+				return;
 			}
-			return;
+		} else {
+			log.info("First access, creating token.");
 		}
 		filterChain.doFilter(request, response);
 	}
